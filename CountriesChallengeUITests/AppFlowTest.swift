b6e40e7c2d7cb4f2df03488fc4b118d946d78857
leoli7405@gmail.com <leoli7405@gmail.com>
2025-05-09 19:56:04 -0700

    finished assignment

--- CountriesChallengeUITests/AppFlowTest.swift
+++ CountriesChallengeUITests/AppFlowTest.swift
@@ -84,7 +84,7 @@ final class AppFlowTest: XCTestCase {
         return []
     }
     
-    func validate_every_country_cell(in app: XCUIApplication, startingIndex: Int, offset: Int = 10) async {
+    func validate_every_country_cell(in app: XCUIApplication, startingIndex: Int, offset: Int = 10) {
         
         all_countries = manually_import_countries()
         let maxSwipes = 5
@@ -109,12 +109,12 @@ final class AppFlowTest: XCTestCase {
             }
             
             XCTAssertTrue(current_element.exists && current_element.isHittable, "Country element \(currentCountry.name) not found after \(maxSwipes) swipes.")
-            await validate_cell_details(el: current_element, currentCountry: currentCountry)
+            validate_cell_details(el: current_element, currentCountry: currentCountry)
         }
     }
     
-    func validate_cell_details(el: XCUIElement, currentCountry: Country) async {
-        let exists = await el.waitForExistence(timeout: 5)
+    func validate_cell_details(el: XCUIElement, currentCountry: Country) {
+        let exists = el.waitForExistence(timeout: 5)
         if exists && el.isHittable {
             DispatchQueue.main.async {
                 el.tap()
@@ -123,9 +123,9 @@ final class AppFlowTest: XCTestCase {
             XCTFail("Element not tappable")
         }
         
-        let details1 = await app?.staticTexts["\(currentCountry.name), \(currentCountry.region)"]
-        let details2 = await app?.staticTexts["\(currentCountry.code)"]
-        let details3 = await app?.staticTexts["\(currentCountry.capital)"]
+        let details1 = app?.staticTexts["\(currentCountry.name), \(currentCountry.region)"]
+        let details2 = app?.staticTexts["\(currentCountry.code)"]
+        let details3 = app?.staticTexts["\(currentCountry.capital)"]
         
         // validate details
         XCTAssertTrue(details1!.exists && details1!.isHittable)
@@ -141,29 +141,82 @@ final class AppFlowTest: XCTestCase {
       if we want to cover entire list, just keep changing index until index == offset
       etc) for startingIndex in 0...offset -> validate_every_country_cell(in: app!, startingIndex: startingIndex)
     */
-    func test_all_countries() async throws {
+    func test_all_countries() throws {
         
-        await validate_every_country_cell(in: app!, startingIndex: 0, offset: 20)
+        validate_every_country_cell(in: app!, startingIndex: 0, offset: 20)
         
         /* below should cover every single country */
-        //await validate_every_country_cell(in: app!, startingIndex: 1, offset: 20)
-        //await validate_every_country_cell(in: app!, startingIndex: 2, offset: 20)
-        //await validate_every_country_cell(in: app!, startingIndex: 3, offset: 20)
-        //await validate_every_country_cell(in: app!, startingIndex: 4, offset: 20)
-        //await validate_every_country_cell(in: app!, startingIndex: 5, offset: 20)
-        //await validate_every_country_cell(in: app!, startingIndex: 6, offset: 20)
-        //await validate_every_country_cell(in: app!, startingIndex: 7, offset: 20)
-        //await validate_every_country_cell(in: app!, startingIndex: 8, offset: 20)
-        //await validate_every_country_cell(in: app!, startingIndex: 9, offset: 20)
-        //await validate_every_country_cell(in: app!, startingIndex: 10, offset: 20)
-        //await validate_every_country_cell(in: app!, startingIndex: 11, offset: 20)
-        //await validate_every_country_cell(in: app!, startingIndex: 12, offset: 20)
-        //await validate_every_country_cell(in: app!, startingIndex: 13, offset: 20)
-        //await validate_every_country_cell(in: app!, startingIndex: 14, offset: 20)
-        //await validate_every_country_cell(in: app!, startingIndex: 15, offset: 20)
-        //await validate_every_country_cell(in: app!, startingIndex: 16, offset: 20)
-        //await validate_every_country_cell(in: app!, startingIndex: 17, offset: 20)
-        //await validate_every_country_cell(in: app!, startingIndex: 18, offset: 20)
-        //await validate_every_country_cell(in: app!, startingIndex: 19, offset: 20)
+        //validate_every_country_cell(in: app!, startingIndex: 1, offset: 20)
+        //validate_every_country_cell(in: app!, startingIndex: 2, offset: 20)
+        //validate_every_country_cell(in: app!, startingIndex: 3, offset: 20)
+        //validate_every_country_cell(in: app!, startingIndex: 4, offset: 20)
+        //validate_every_country_cell(in: app!, startingIndex: 5, offset: 20)
+        //validate_every_country_cell(in: app!, startingIndex: 6, offset: 20)
+        //validate_every_country_cell(in: app!, startingIndex: 7, offset: 20)
+        //validate_every_country_cell(in: app!, startingIndex: 8, offset: 20)
+        //validate_every_country_cell(in: app!, startingIndex: 9, offset: 20)
+        //validate_every_country_cell(in: app!, startingIndex: 10, offset: 20)
+        //validate_every_country_cell(in: app!, startingIndex: 11, offset: 20)
+        //validate_every_country_cell(in: app!, startingIndex: 12, offset: 20)
+        //validate_every_country_cell(in: app!, startingIndex: 13, offset: 20)
+        //validate_every_country_cell(in: app!, startingIndex: 14, offset: 20)
+        //validate_every_country_cell(in: app!, startingIndex: 15, offset: 20)
+        //validate_every_country_cell(in: app!, startingIndex: 16, offset: 20)
+        //validate_every_country_cell(in: app!, startingIndex: 17, offset: 20)
+        //validate_every_country_cell(in: app!, startingIndex: 18, offset: 20)
+        //validate_every_country_cell(in: app!, startingIndex: 19, offset: 20)
     }
+    
+    func test_search_functionality_1() throws {
+        let countryToSearch = Country(
+            capital: "Andorra la Vella",
+            code: "AD",
+            currency: Currency(code: "EUR", name: "Euro", symbol: "€"),
+            flag: "https://restcountries.eu/data/and.svg",
+            language: Language(code: "ca", name: "Catalan"),
+            name: "Andorra",
+            region: "EU"
+        )
+        let searchField = app!.searchFields.firstMatch
+        let searchFieldExists = searchField.exists;
+        XCTAssertTrue(searchFieldExists, "Search bar not found")
+        
+        DispatchQueue.main.async {
+            searchField.tap()
+            searchField.typeText(countryToSearch.capital)
+            sleep(1)
+        }
+        
+        let el = app!.staticTexts["\(countryToSearch.name), \(countryToSearch.region)"]
+        let searchResult = el.waitForExistence(timeout: 5)
+        XCTAssertTrue(searchResult)
+        validate_cell_details(el: el, currentCountry: countryToSearch)
+    }
+    
+    func test_search_functionality_2() throws {
+        let countryToSearch = Country(
+            capital: "Washington, D.C.",
+            code: "US",
+            currency: Currency(code: "USD", name: "United States dollar", symbol: "$"),
+            flag: "https://restcountries.eu/data/usa.svg",
+            language: Language(code: "ca", name: "English"),
+            name: "United States of America",
+            region: "NA"
+        )
+        let searchField = app!.searchFields.firstMatch
+        let searchFieldExists = searchField.exists;
+        XCTAssertTrue(searchFieldExists, "Search bar not found")
+        
+        DispatchQueue.main.async {
+            searchField.tap()
+            searchField.typeText(countryToSearch.capital)
+            sleep(1)
+        }
+        
+        let el = app!.staticTexts["\(countryToSearch.name), \(countryToSearch.region)"]
+        let searchResult = el.waitForExistence(timeout: 5)
+        XCTAssertTrue(searchResult)
+        validate_cell_details(el: el, currentCountry: countryToSearch)
+    }
+
 }
